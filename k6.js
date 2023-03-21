7ea50f2f20f0533b022ae0b8dba54ee3ed74f10e
Oleg Zimakov <ozimakov@redhat.com>
2023-03-20 17:38:15 -0700

    knative serving added

--- k6.js
+++ k6.js
@@ -1,16 +1,18 @@
-import { check } from 'k6';
+import {check} from 'k6';
 import http from 'k6/http';
 
 export const options = {
     stages: [
-		{ duration: '2m',  target: 100 },
-        { duration: '3m',  target: 100 },
-        { duration: '1m',  target: 0 },
+        { duration: '30s', target: 1   },
+        { duration: '2m',  target: 200 },
+        { duration: '2m',  target: 50  },
+        { duration: '2m',  target: 300 },
+        { duration: '1m',  target: 0   }
     ],
 };
 
 export default function () {
-    const res = http.get(`${__ENV.PI_ENDPOINT}/pi/7`);
+    const res = http.get(`${__ENV.PI_ENDPOINT}/pi/6`);
     check(res, {
         'is status 200': (r) => r.status === 200,
     });
