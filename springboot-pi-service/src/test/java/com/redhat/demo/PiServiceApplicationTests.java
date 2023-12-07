61d3bfa789484218d0dbd3bd8167721a6f64cde2
Oleg Zimakov (Red Hat) <107713584+ozimakov-rh@users.noreply.github.com>
2023-12-06 16:23:11 -0800

    Upgrade to Spring Boot 3 with native build (#2)

--- springboot-pi-service/src/test/java/com/redhat/demo/PiServiceApplicationTests.java
+++ springboot-pi-service/src/test/java/com/redhat/demo/PiServiceApplicationTests.java
@@ -2,17 +2,12 @@ package com.redhat.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
